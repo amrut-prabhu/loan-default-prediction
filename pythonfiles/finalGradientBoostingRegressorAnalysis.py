fff5db06e45476fb4c1f05b9c6b3106ed66f03db
SIPANI SREYANS <ssipani@xcnd50.comp.nus.edu.sg>
2018-11-09 15:20:33 +0800

    Remove city column

--- pythonfiles/finalGradientBoostingRegressorAnalysis.py
+++ pythonfiles/finalGradientBoostingRegressorAnalysis.py
@@ -6,9 +6,12 @@ from sklearn.multioutput import RegressorChain
 from sklearn.multioutput import MultiOutputRegressor 
 from sklearn.model_selection import train_test_split
 
+
+pd.set_option('display.max_columns', 500)
+
 df = pd.read_csv('https://drive.google.com/uc?export=download&id=1XoV8SfvHmzaxRuDRe81OWSQu10dYTbO5',sep=',')
 
-df_X = df.iloc[:, 1:12].copy()
+df_X = df.iloc[:, 2:12].copy()
 df_X = pd.get_dummies(df_X)
 
 df_y1 = df.iloc[:, 12:15].copy()
@@ -16,6 +19,8 @@ df_y1 = pd.get_dummies(df_y1)
 
 df_y2 = df.iloc[:, 15:19].copy()
 df_y2 = pd.get_dummies(df_y2)
+
+print(df_X.head())
 
 X_train, X_test, y_train, y_test = train_test_split(df_X, df_y1, test_size=0.2, random_state=0)
 
@@ -33,20 +38,20 @@ model1 = RegressorChain(model)
 model2 = MultiOutputRegressor(model, n_jobs = -1)
 model1.fit(X_train, y_train)
 joblib.dump(model1, "RegressorChainGradientBoostingRegressorEarnings.pkl")
-print("Model 1: ");
-accuracy(model1, X_test, y_test)
-model2.fit(X_train, y_train)
-joblib.dump(model2, "MultipleOutputGradientBoostingRegressorEarnings.pkl")
-print("Model 2: ");
-accuracy(model2, X_test, y_test)
+#print("Model 1: ");
+#accuracy(model1, X_test, y_test)
+#model2.fit(X_train, y_train)
+#joblib.dump(model2, "MultipleOutputGradientBoostingRegressorEarnings.pkl")
+#print("Model 2: ");
+#accuracy(model2, X_test, y_test)
 
 X_train, X_test, y_train, y_test = train_test_split(df_X, df_y2, test_size=0.2, random_state=0)
 
 model1.fit(X_train, y_train)
 joblib.dump(model1, "RegressorChainGradientBoostingRegressorRepayment.pkl")
-print("Model 3: ");
-accuracy(model1, X_test, y_test)
-model2.fit(X_train, y_train)
-joblib.dump(model2, "MultipleOutputGradientBoostingRegressorRepayment.pkl")
-print("Model 4: ");
-accuracy(model2, X_test, y_test)
+#print("Model 3: ");
+#accuracy(model1, X_test, y_test)
+#model2.fit(X_train, y_train)
+#joblib.dump(model2, "MultipleOutputGradientBoostingRegressorRepayment.pkl")
+#print("Model 4: ");
+#accuracy(model2, X_test, y_test)
